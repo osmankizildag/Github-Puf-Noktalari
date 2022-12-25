f431868e3eb1ec8de72990ffa731e1292dbd2801
osman <buchhalterkizildag@gmail.com>
2022-12-25 15:26:14 +0100

    25/12/22 15:26

---
+++ src/GithubaGiris/GithubPratikNotlarBurada.java
@@ -0,0 +1,27 @@
+package GithubaGiris;
+
+public class GithubPratikNotlarBurada {
+
+      /*
+    1- öncelikle InteliJ'de bir proje olusturulur   (ben desktopta olmasını tercih ediyorum)
+    2- inteliJ Terminaline gidilir.
+    3- git init yazılır     (her projede sadece bir defa yapılıyor)
+    4- projede kod vs calısmaları yapılır
+    5- olusturulan classların rengi kırmızı görunur
+    6- git add . ile kodlar local yani kendi bilgisayarımızaki repoya yuklenir
+       olusturulan veya degısıklık yapılan class renkleri yeşile döner
+    7- git commit -m"25/12/2022 15:22        (buraya yazı olarak tarih ve saat yazmayı tercih ediyorum)
+       class rengi siyaha döner
+    8- github a gidilip yeni repository olusturulur
+        (ismi inteliJ"deki dosya ismimizle aynı olması sart değil)
+        (asağıdaki 9, 10 ve 11. satırlar, terminale sırasıyla kopyalanıp yapıstırılır)
+    9- git branch -M main
+    10- git remote add origin https://github.com/osmankizildag/Github-Puf-Noktalari.git
+    11- git push -u origin main     (problem cıkrsa -u yerine -f deneyin)
+    12- işlem tamam     (alıcılar hazır, github a dosya gönderebilirsiniz)
+    13- git push yap
+        dosyalarımızın github a gönderildiğini kontrol etmek için sayfa yenilem tusuna basın
+        Hayırlı olsun
+     */
+
+}
